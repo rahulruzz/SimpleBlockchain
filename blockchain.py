a4f295625c9ead35096b3190ef99a22492f57165
Rahul Singh <rahulruzzsingh@gmail.com>
2020-01-26 22:01:50 +1100

    Updated new_block and new_transaction
    Self in functions: specifies this function for instance usage. When a new object is created, it does not create a new set of instance methods. Instances methods are retained within the class object (nice way to save memory). Hence why self is used to determine which instance the function will apply too.
    i.e.
    obj1 = Class()
    obj2 = Class()
    obj1.add_to_arr(2)
    This translate to:
    Class.add_to_arr(obj1, 2)
    Pass: null command.

--- blockchain.py
+++ blockchain.py
@@ -1,15 +1,52 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
-        self.current_transaction = []
+        self.current_transactions = []
 
-    def new_block(self):
-        # Creates new block and adds to chain
-        pass
+        # Genesis
+        # Initial block of our block chain
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        Creates a new block in the blockchain
+        :param proof: <int> The proof (using Proof of Work algorithm)
+        :param previous_hash: (Optional) <str> Hash of previous block
+        :return: <dict> New block
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transaction': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        # Reset current transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
-        # Adds a new transaction to a list of transactions
-        pass
+        """
+        Creates a new transaction. Adds to next new block to be minded
+        :param sender: <string> Address of sender
+        :param recipient: <string> Address of recipient
+        :param amount: <int> Amount
+        :return: <int> Index of block that holds this transaction
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
