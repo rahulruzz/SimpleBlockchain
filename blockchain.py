325b15170ec8d0fadedebff75b705f43156d82cb
Rahul Singh <rahulruzzsingh@gmail.com>
2020-01-26 22:27:16 +1100

    Added proof of work basis

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,8 @@
 import hashlib
 import json
+
 from time import time
+from uuid import uuid4
 
 class Blockchain(object):
     def __init__(self):
@@ -48,12 +50,48 @@ class Blockchain(object):
 
         return self.last_block['index'] + 1
 
+    def proof_of_work(self, last_proof):
+        """
+        Simple proof of work:
+        Find a number p such that hash(pp*) contains 4 leading zeros, where p is the previous
+        proof and p* is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        # Simple incrementing while, trying to find a successful proof
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates proof
+        :param last_proof: <int> Previous proof
+        :param proof: <int> Current proof
+        :return: <bool> True if correct, False if not
+        """
+        # Convert to unicode
+        guess = f'{last_proof}{proof}'.encode()
+        # Hash the guess
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        # Does it match?
+        return guess_hash[:4] == "0000"
+
     @staticmethod
     def hash(block):
-        # Hashes a block
-        pass
+        """
+        Creates a SHA-256 hash of a block
+        :param block: <dict> block
+        :return: <string>
+        """
+        # Need to order dictionary to prevent inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(bloock_string).hexdigest()
 
     @property
     def last_block(self):
         # Returns the last block in the chain
-        pass
+        return self.chain[-1]
